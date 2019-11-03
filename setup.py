5709b187610ffdec7b5da6dcd5099209b0921504
Peter Jipsen <jipsen@chapman.edu>
2019-11-03 11:03:36 -0800

    added interpformat and commented out make vampire

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import subprocess
 class BuildPy(build_py):
     def run(self):
         subprocess.call(['make', 'prover9'], cwd='provers/bin')
-        subprocess.call(['make', 'vampire'], cwd='provers/bin')
+        #subprocess.call(['make', 'vampire'], cwd='provers/bin')
         subprocess.call(['make', 'eprover'], cwd='provers/bin')
         build_py.run(self)
 
@@ -61,6 +61,7 @@ setup(
     package_data={
         'provers': [
             'bin/isofilter',
+            'bin/interpformat',
             'bin/prooftrans',
             'bin/tptp_to_ladr',
             'bin/ladr_to_tptp',
